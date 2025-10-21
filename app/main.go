d703b7f10f54f0af0a90c980a1ce50eb4b976557
Harsh Agarwal <harsh.agarwal@devrev.ai>
2025-10-21 22:18:54 +0530

    send correlation id

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
@@ -13,9 +14,32 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	reqData := make([]byte, 1024) 
+	n, err := conn.Read(reqData)
+	if err != nil {
+		fmt.Println("Error reading data: ", err.Error())
+		os.Exit(1)
+	}
+	reqData = reqData[:n] 
+
+	resData := make([]byte, 8)
+	
+	messageSize := int32(0)
+	binary.BigEndian.PutUint32(resData[0:4], uint32(messageSize))
+	
+	correlationId := int32(7)
+	binary.BigEndian.PutUint32(resData[4:8], uint32(correlationId))
+	
+	_, err = conn.Write(resData)
+	if err != nil {
+		fmt.Println("Error writing data: ", err.Error())
 		os.Exit(1)
 	}
 }
